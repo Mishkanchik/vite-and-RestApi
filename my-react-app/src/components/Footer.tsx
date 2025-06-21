56ae733c459e295d394df937183213da50668d52
mishkanchik <98422930+Mishkanchik@users.noreply.github.com>
2025-06-21 21:54:16 +0300

    Delete my-react-app directory

--- my-react-app/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="bg-indigo-900 text-white text-center py-4 mt-8">
-    <p>© 2025 MyApp. Усі права захищені.</p>
-  </footer>
-);
-
-export default Footer;
